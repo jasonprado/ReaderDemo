54d9ed619b8068b11d1546504a8afd731cf538b6
Jason Prado <jason.prado@gmail.com>
2023-08-26 12:20:26 -0700

    Initial Commit

---
+++ ReaderDemo/ReaderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderDemoApp.swift
+//  ReaderDemo
+//
+//  Created by Jason Prado on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ReaderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
